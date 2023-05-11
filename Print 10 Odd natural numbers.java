86342a2e4de070132d5881b8e395c628c72140e4
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-05-12 02:11:02 +0530

    This program is used to print first 10 odd natural number through for loop

---
+++ Print 10 Odd natural numbers.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 10;
+        int i ;
+        for( i=0 ; i <=10 ; i++){
+            System.out.println(2*i+1);
+        }
+
+    }
+}
