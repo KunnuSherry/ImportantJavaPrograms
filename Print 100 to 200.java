831046c2c72e98a84e7de0ed04cadb594972353e
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-05-12 01:51:34 +0530

    Program to print natural numer from 1 to 200

---
+++ Print 100 to 200.java
@@ -0,0 +1,10 @@
+
+ public class Main {
+    public static void main(String[] args) {
+        int i = 100;
+        while (i<=200) {
+            System.out.println(i);
+            i++ ;
+        }
+    }
+}
