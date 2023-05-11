48dc536e87c9178cef4c773fe65990acce9d8f91
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-05-12 02:52:15 +0530

    This is a program to calculate the sum of the numbers occurring in the multiplication table of 8.

---
+++ Add Table of 8.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 8;
+        int i = 1 ;
+        int sum = 0;
+
+        while (i<=10){
+
+            sum  += a*i;
+            i++ ;
+
+            }
+        System.out.println(sum);
+
+        }
+
+
+
+
+
+
+}
