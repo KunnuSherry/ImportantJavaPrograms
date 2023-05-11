cc92e4d301a2b474fa348848211416c59a24ddf5
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-05-12 01:37:06 +0530

    This program is used for Encrypting and Decrypting

---
+++ EncryptDecrypt.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        char grade = 'b' ;                         // encrypting
+        grade = (char)(grade + 8);
+        System.out.println(grade);
+        grade = (char)(grade - 8);                 // decrypting
+        System.out.println(grade);
+
+
+
+
+
+
+    }
+}
