903498d7e787e65e214b365ba31fac1379e71907
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-05-12 01:41:07 +0530

    This program is used to covert the any string given by the user to lower case

---
+++ Convert To Lower Case.java
@@ -0,0 +1,12 @@
+
+import java.util.Locale;
+import java.util.Scanner ;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("write your string:");
+        String a = sc.next();
+        a = a.toLowerCase();
+        System.out.println(a);
+    }
+}
