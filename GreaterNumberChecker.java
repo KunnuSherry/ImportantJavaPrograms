499c64f2cb75ab91216dc6ad6e805407e1046481
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-05-12 01:39:35 +0530

    Create GreaterNumberChecker.java
    This program will check if your entered integer is greater than 8

---
+++ GreaterNumberChecker.java
@@ -0,0 +1,10 @@
+import java.util.Scanner ;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter you number:");
+        float a = sc.nextFloat();
+        System.out.println(a>8);
+
+    }
+}
