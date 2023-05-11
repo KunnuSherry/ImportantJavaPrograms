24f6d565468617ba0a7b0e595d9471d3fd844d3b
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-05-12 02:33:49 +0530

    This Program is used to Print first n even number through while loop

---
+++ Print N Even Numbers.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int n = 8 ;
+        int i = 0 ;
+        while(i<=n){
+            System.out.println(2*i);
+            i++ ; 
+        }
+
+
+    }
+
+}
