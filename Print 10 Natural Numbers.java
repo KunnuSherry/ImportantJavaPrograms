79dfe5e8dda67b7a06b7657be471f54626202135
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-05-12 02:04:58 +0530

    This program is used to print 10 natural numbers by using do-while loop

---
+++ Print 10 Natural Numbers.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 10 ;
+        int i =1 ;
+        do {
+            System.out.println(i);
+            i++ ;
+        } while (i<=10) ;
+    }
+}
