e6ef8acaa2c9d6dce05a6659c7cad1f918398efe
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-05-12 01:44:35 +0530

    Create Strings Detecting.java
    This Program Is Used to detect double and triple spaces

---
+++ Strings Detecting.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        String a = "k  u   n a l" ;
+        System.out.println(a.indexOf("  "));
+        System.out.println(a.indexOf("   ")) ;
+
+    }
+}
