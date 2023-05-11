100a84d8952e729f4d45177b9dd2ee0dd2a176ba
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-05-12 02:38:06 +0530

    This program is used to write the table of 10 in reverse

---
+++ Table of 10 in Reverse.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int i = 1 ;
+        while (i<=a){
+            System.out.println(a*(a-i+1));
+            i++;
+        }
+
+
+    }
+
+}
