ace08090624a09a0b93f57e1227e253b9c0d55e2
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-05-12 01:35:23 +0530

    A program can be called as Integer checker

---
+++ CheckIntegralValue.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter any number :");
+        Boolean a = sc.hasNextInt();
+        System.out.println("Is the number integer ? " + a);
+
+    }
+}
