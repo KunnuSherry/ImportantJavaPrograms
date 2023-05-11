78dca29053709ad9d492821e4f2d519cda1b1e08
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-05-12 01:27:56 +0530

    This program is used to add three numbers that are given by the user

---
+++ AddingThreeNumbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 2;
+        int number2 = 3;
+        int number3 = 4;
+        int sum = number1 + number2 + number3;
+        System.out.println(sum);
+    }
+}
