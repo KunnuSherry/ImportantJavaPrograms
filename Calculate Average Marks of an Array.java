08dbe421003d9c6cf90569af9717414576c403cf
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-05-13 19:16:32 +0530

    This is a program Calculate the average marks from an array containing marks of all students in physics using a for-each loop

---
+++ Calculate Average Marks of an Array.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] marks = {100, 90, 80, 70, 60}; // these are the marks of 5 students
+        int sum = 0;
+        for(int element:marks){
+            sum = sum + element ;
+        }
+        System.out.println("The average marks is : " + sum/5);
+
+
+
+
+
+
+
+    }
+}
