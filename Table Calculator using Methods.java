8dc13d8b191b810fd16dfb694ee8150705bbb6cf
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-05-18 18:14:36 +0530

    This is a program to make a Table Calculator using Methods.java

---
+++ Table Calculator using Methods.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    static void mul(int n){
+        for(int i=1; i<=10 ; i++){
+            System.out.println(n+"x"+i+"="+n*i);
+        }
+    }
+
+    public static void main(String[] args) {
+        mul(8);
+    }
+
+
+}
