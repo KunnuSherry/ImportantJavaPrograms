eb092bb50aa9d7ad287ebcfceae7880486edb48d
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-05-22 22:38:12 +0530

    This program is to create a class circle and use inheritance to create another class cylinder from it and find its volume.

---
+++ Inheritance.java
@@ -0,0 +1,36 @@
+package org.example;
+
+class Circle{
+    public int radius;
+    Circle(){
+        System.out.println("I am non param of circle");
+    }
+    Circle(int r){
+        System.out.println("I am circle parameterized constructor");
+        this.radius = r;
+    }
+
+    public double area(){
+        return Math.PI*this.radius*this.radius;
+    }
+}
+
+class Cylinder1 extends Circle{
+    public int height;
+    Cylinder1(int r, int h){
+        super(r);
+        System.out.println("I am cylinder1 parameterized constructor");
+        this.height = h;
+    }
+    public double volume(){
+        return Math.PI*this.radius*this.radius*this.height;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        // Problem 1
+        // Circle objC = new Circle(12);
+        Cylinder1 obj = new Cylinder1(12, 4);
+        System.out.println(obj.volume());
+    }
+}
