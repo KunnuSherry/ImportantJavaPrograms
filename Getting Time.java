80fed4c93f04a7adedd993425b0fc1458536d9b4
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-06-15 20:59:42 +0530

    This is a java program to print time in the format 20:00:00

---
+++ Getting Time.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args) {
+        Date d = new Date();
+        System.out.println(d.getHours()+":"+d.getMinutes()+":"+d.getSeconds());
+    }
+}
