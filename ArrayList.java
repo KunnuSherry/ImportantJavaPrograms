d927ad21408c893a94aacc3befeeb2c1635b51a4
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-06-15 20:55:05 +0530

    This is a program to make an ArrayList and store the names of two students inside it. Print it using a for each loop

---
+++ ArrayList.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList ar = new ArrayList();
+        ar.add("student 1");
+        ar.add("student 2");
+        System.out.println(ar);
+    }
+}
