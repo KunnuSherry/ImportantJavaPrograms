45152b927d1ba00adf68674864d840d5eb8301ec
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-09-15 22:17:40 +0530

    Create Even Numbers Print.java
    Print all even numbers till n.

---
+++ Even Numbers Print.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+//        int i = 1 ;
+//        while(i<=n){
+//            System.out.println(i*2);
+//            i++;
+//        }
+        for(int i=1; i<=n; i++){
+            System.out.println(i*2);
+        }
+    }
+}
