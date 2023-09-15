a8a4a3f0ca62146fce40ff088af185131283b71a
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-09-15 22:24:19 +0530

    Create Infinte Loop.java
    loop on your system and analyze what happens. Try to think of the reason for the output produced.

---
+++ Infinte Loop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+        for(; ;){
+            System.out.println("I love coding");
+        }
+    }
+}
+/** This is an infinite loop since we have not given any condition in the for loop **/
