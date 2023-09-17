87a7588e6a222805e70483a11f71a6c84665b3b8
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-09-17 22:30:00 +0530

    Create 7th.java

---
+++ Patterns/7th.java
@@ -0,0 +1,18 @@
+class main {
+    public static void main(String[] args) {
+        for (int i=1; i<=5; i++){
+            for (int j=1; j<=6-i; j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+
+
+    }
+}
+
+// 12345
+// 1234
+// 123
+// 12
+// 1
